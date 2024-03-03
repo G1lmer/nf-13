220e123b6c9fa6af7e429717b91b5c0e0b26ff00
Serhii_Movenko <Serhii_Movenko@EPUAFRAW008E>
2024-03-03 16:07:15 +0200

    Update tests

--- tests/calculator_test.cpp
+++ tests/calculator_test.cpp
@@ -2,7 +2,7 @@
 #include <Calculator.h>
 #include <SymbolOperationMApper.h>
 
-TEST(MultiplyTests, TestOne)
+TEST(CalculatorTests, MinusTest)
 {
 	Calculator calculator = Calculator(new SymbolOperationMapper());
 	double a = 7.1;
@@ -14,7 +14,7 @@ TEST(MultiplyTests, TestOne)
 	
 }
 
-TEST(MultiplyTests, TestTwo)
+TEST(CalculatorTests, PlusTest)
 {
 	Calculator calculator = Calculator(new SymbolOperationMapper());
 	double a = 7.1;
@@ -25,3 +25,27 @@ TEST(MultiplyTests, TestTwo)
 	ASSERT_EQ(10.5, plusResult);
 
 }
+
+/**TEST(CalculatorTests, MultiplyTest)
+{
+	Calculator calculator = Calculator(new SymbolOperationMapper());
+	double a = 2;
+	double b = 3;
+
+	double multiplyResult = calculator.calculate(a, b, "*");
+
+	ASSERT_EQ(2 * 3, multiplyResult);
+
+}**/
+
+/**TEST(CalculatorTests, DivideTest)
+{
+	Calculator calculator = Calculator(new SymbolOperationMapper());
+	double a = 6;
+	double b = 3;
+
+	double divideResult = calculator.calculate(a, b, "/");
+
+	ASSERT_EQ(6 / 3, divideResult);
+
+}**/
